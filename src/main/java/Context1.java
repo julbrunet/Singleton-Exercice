926b16a19e476efc325757dc7b509ee2a2a1102a
Julien Brunet <julien.brunet@cegeplimoilou.ca>
2025-04-07 20:48:46 -0400

    Initial commit

---
+++ src/main/java/Context1.java
@@ -0,0 +1,10 @@
+package PACKAGE_NAME;
+
+
+/**
+ * Singleton-Exercice 
+ * @author Julien.Brunet 
+ * @since 2025-04-07
+ */
+public class Context1 {
+}
---
+++ src/main/java/Context2.java
@@ -0,0 +1,16 @@
+/**
+ * Singleton-Exercice
+ *
+ * @author Julien.Brunet
+ * @since 2025-04-07
+ */
+public class Context1 {
+
+    public Context1(){
+
+    }
+
+    public void start(){
+
+    }
+}
